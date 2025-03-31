4735da751fd962698816a0d1b59363bd53354831
Adam Mirza <amirza495@gmail.com>
2025-03-31 08:24:49 -0400

    configure tab settings for some files

--- dot-config/nvim/lua/config/opts.lua
+++ dot-config/nvim/lua/config/opts.lua
@@ -9,3 +9,26 @@ vim.opt.fixeol = true
 --	virtual_text = true,
 --})
 --vim.diagonstic.config({ virtual_text = { severity = { min = vim.diagonsitc.severity.HINT, max = vim.diagnostic.seeverity.ERROR },},)
+
+-- configure tab width and expansions for different languages
+vim.api.nvim_create_augroup("FileTypeSpecificTabs", { clear = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "python",
+	command = "setlocal shiftwidth=4 tabstop=4 expandtab",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "lua",
+	command = "setlocal shiftwidth=2 tabstop=2 expandtab",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "cpp",
+	command = "setlocal shiftwidth=2 tabstop=2 expandtab",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "rust",
+	command = "setlocal shiftwidth=4 tabstop=4 expandtab",
+})
